78d5a33e15e30b1a498356c05877945331fe5ff8
Patrick <tshupane@outlook.com>
2023-02-21 18:05:51 -0800

    isalpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  * _islower - check the code
  * description - write a program that checks for lowercase characters
  * @c: the character to be checked
- * return: 1 if @c is lowercase.
+ * return: 1 if @c is lowercase
  */
 int _islower(int c)
 {

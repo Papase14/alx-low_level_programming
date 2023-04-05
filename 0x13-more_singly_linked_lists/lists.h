30d88c306bbc34aa337d91ce5cfba51c73eb7dbb
PatWork <tshupane@outlook.com>
2023-04-04 18:45:48 -0700

    Sinngly Linked List

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include <stlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer

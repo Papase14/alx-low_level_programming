40f52085c637ad52c1c89d05af270527c5f9b1fc
PatWork <tshupane@outlook.com>
2023-04-04 17:54:09 -0700

    More Singly Linked Lists Task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,12 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t sizenodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nodes++;
-	} return (nodes);
+		sizenodes++;
+	}
+	return (sizenodes);
 }
